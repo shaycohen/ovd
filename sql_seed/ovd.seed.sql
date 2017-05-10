d91ac94d0f5be6a3ba3ec39c9d733ac0f0cabf09
Shay Cohen <shay.cohen@sap.com>
2017-05-10 11:58:49 +0300

    updated DB structure

--- sql_seed/ovd.seed.sql
+++ sql_seed/ovd.seed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Generation Time: May 07, 2017 at 12:36 PM
+-- Generation Time: May 10, 2017 at 07:41 AM
 -- Server version: 5.7.18
 -- PHP Version: 7.0.16
 
@@ -30,9 +30,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `container` (
   `id` int(11) NOT NULL,
-  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `warehouse_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `container` (`id`, `description`, `warehouse_id`) VALUES
+(1, 'cont1', 1);
+INSERT INTO `container` (`id`, `description`, `warehouse_id`) VALUES
+(2, 'cont2', 2);
+INSERT INTO `container` (`id`, `description`, `warehouse_id`) VALUES
+(3, 'cont3', 1);
+INSERT INTO `container` (`id`, `description`, `warehouse_id`) VALUES
+(4, 'cont4', 2);
 -- --------------------------------------------------------
 
 --
@@ -49,26 +58,6 @@ CREATE TABLE `report` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reports`
---
-
-CREATE TABLE `reports` (
-  `id` int(11) NOT NULL,
-  `reporter` varchar(255) NOT NULL,
-  `container` varchar(255) NOT NULL,
-  `note` varchar(255) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `reports`
---
-
-INSERT INTO `reports` (`id`, `reporter`, `container`, `note`) VALUES
-(1, 'reporter1', 'containerA', 'bump');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `user`
 --
 
@@ -76,8 +65,15 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `fname` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `lname` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `warehouse_id` int(11) NOT NULL
+  `username` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `warehouse_id` int(11) NOT NULL,
+  `pw` varchar(2048) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `user` (`id`, `fname`, `lname`, `username`, `warehouse_id`, `pw`) VALUES
+(1, 'user', 'one', 'user1', 1, 'pass1');
+INSERT INTO `user` (`id`, `fname`, `username`, `lname`, `warehouse_id`, `pw`) VALUES
+(2, 'user', 'two', 'user2', 2, 'pass2');
 
 -- --------------------------------------------------------
 
@@ -90,15 +86,14 @@ CREATE TABLE `warehouse` (
   `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `warehouse_containers`
+-- Dumping data for table `warehouse`
 --
 
-CREATE TABLE `warehouse_containers` (
-  `container_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `warehouse` (`id`, `description`) VALUES
+(1, 'warehouse1');
+INSERT INTO `warehouse` (`id`, `description`) VALUES
+(2, 'warehouse2');
 
 --
 -- Indexes for dumped tables
@@ -108,7 +103,8 @@ CREATE TABLE `warehouse_containers` (
 -- Indexes for table `container`
 --
 ALTER TABLE `container`
-  ADD UNIQUE KEY `container_uniq` (`id`);
+  ADD UNIQUE KEY `container_uniq` (`id`),
+  ADD KEY `warehouse_id` (`warehouse_id`);
 
 --
 -- Indexes for table `report`
@@ -119,16 +115,11 @@ ALTER TABLE `report`
   ADD KEY `user_container_id` (`user_id`);
 
 --
--- Indexes for table `reports`
---
-ALTER TABLE `reports`
-  ADD UNIQUE KEY `id` (`id`);
-
---
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD UNIQUE KEY `user_uniq` (`id`),
+  ADD UNIQUE KEY `username_uniq` (`username`),
   ADD KEY `user_warehouse_id` (`warehouse_id`);
 
 --
@@ -138,23 +129,14 @@ ALTER TABLE `warehouse`
   ADD UNIQUE KEY `warehouse_uniq` (`id`);
 
 --
--- Indexes for table `warehouse_containers`
---
-ALTER TABLE `warehouse_containers`
-  ADD KEY `warehouse_container_id` (`container_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `reports`
---
-ALTER TABLE `reports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
---
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `container`
+--
+ALTER TABLE `container`
+  ADD CONSTRAINT `warehouse_id` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse` (`id`);
 
 --
 -- Constraints for table `report`
@@ -168,12 +150,6 @@ ALTER TABLE `report`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `user_warehouse_id` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse` (`id`);
-
---
--- Constraints for table `warehouse_containers`
---
-ALTER TABLE `warehouse_containers`
-  ADD CONSTRAINT `warehouse_container_id` FOREIGN KEY (`container_id`) REFERENCES `container` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
