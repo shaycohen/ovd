22405c0437116f91d92aa9243dc2220e1dfb4163
Shay Cohen <shay.cohen@sap.com>
2017-05-10 11:59:50 +0300

    renamed seeding file

--- provision.sh
+++ provision.sh
@@ -66,7 +66,7 @@ docker run --name $NAME-myadmin -d --link $NAME-db:db -p 8080:80 $MYADMIN_IMG
 docker run --rm \
 	-v /vagrant/sql_seed/:/tmp/sql_seed \
 	--link $NAME-db:db $MYSQL_IMG \
-	sh -c 'exec mysql -h"$DB_PORT_3306_TCP_ADDR" -P"$DB_PORT_3306_TCP_PORT" -uroot -p"$DB_ENV_MYSQL_ROOT_PASSWORD" '$NAME' < /tmp/sql_seed/'$NAME'.sample.db' || exit $?
+	sh -c 'exec mysql -h"$DB_PORT_3306_TCP_ADDR" -P"$DB_PORT_3306_TCP_PORT" -uroot -p"$DB_ENV_MYSQL_ROOT_PASSWORD" '$NAME' < /tmp/sql_seed/'$NAME'.seed.sql' || exit $?
 
 docker run -d \
 	--name=$NAME-httpd \
