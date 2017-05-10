5593882bb07d6f4f742aaaba733f4887ebb164aa
Shay Cohen <shay.cohen@sap.com>
2017-05-10 11:59:14 +0300

    created initial GET interfaces

--- html/common.php
+++ html/common.php
@@ -41,7 +41,7 @@ function echoDebug($caller, $text, $level) {
   global $br;
 	if (isset($_GET['debug_level']) && $_GET['debug_level'] > $level) { 
 		echo "DEBUG($level<".$_GET['debug_level'].") [ ". $caller ." ] $br";
-		echo var_dump($text);
+		echo var_dump($text) . $br.$br;
 	}
 }
 
@@ -70,7 +70,7 @@ function db_query($query, $params) {
   if ($_GET[debug_level] > 3) { 
     try { 
       if($stmt->execute($params)) { ;
-        $return = array('fetch'=>$stmt->fetch(), 'rowCount'=>$stmt->rowCount(), 'stmt'=>$stmt);
+        $return = array('fetch'=>$stmt->fetch(), 'stmt'=>$stmt);
         echoDebug('common::db_query::result', $return, 4);
         return $return;
       } else {
@@ -99,7 +99,7 @@ function db_query($query, $params) {
     }
   } else { 
     if ($stmt->execute($params)) {
-      $return = array('fetch'=>$stmt->fetch(), 'rowCount'=>$stmt->rowCount(), 'stmt'=>$stmt);
+      $return = array('fetch'=>$stmt->fetch(), 'stmt'=>$stmt);
       echoDebug('common::db_query::result', $return, 4);
       return $return;
     } else {
@@ -108,3 +108,55 @@ function db_query($query, $params) {
   }
 }
 
+function auth() {
+  $realm = 'OVD';
+
+  if (empty($_SERVER['PHP_AUTH_DIGEST'])) {
+      header('HTTP/1.1 401 Unauthorized');
+      header('WWW-Authenticate: Digest realm="'.$realm.
+             '",qop="auth",nonce="'.uniqid().'",opaque="'.md5($realm).'"');
+
+      die('Not Authorized');
+  }
+
+
+  if (!($data = http_digest_parse($_SERVER['PHP_AUTH_DIGEST']))) { 
+    echoError("common::auth", "AUTH_DIGEST not set");
+    die("AUTH_DIGEST not set");
+  }
+
+  $user = db_query("SELECT * FROM user WHERE username=?", array($data['username']))[fetch];
+  if (!(isset($user['username']))) { 
+    unset($_SERVER["PHP_AUTH_DIGEST"]);
+    die('Missing Credentials');
+  }
+  echoDebug('common:auth::user', $user, 9);
+  return $user;
+
+  $A1 = md5($data['username'] . ':' . $realm . ':' . $user['pw']);
+  $A2 = md5($_SERVER['REQUEST_METHOD'].':'.$data['uri']);
+  $valid_response = md5($A1.':'.$data['nonce'].':'.$data['nc'].':'.$data['cnonce'].':'.$data['qop'].':'.$A2);
+
+  if ($data['response'] != $valid_response)
+      die('Wrong Credentials');
+  echoDebug("common::auth", $data, 3);
+}
+
+
+function http_digest_parse($txt) {
+  // protect against missing data
+  $needed_parts = array('nonce'=>1, 'nc'=>1, 'cnonce'=>1, 'qop'=>1, 'username'=>1, 'uri'=>1, 'response'=>1);
+  $data = array();
+  $keys = implode('|', array_keys($needed_parts));
+
+  preg_match_all('@(' . $keys . ')=(?:([\'"])([^\2]+?)\2|([^\s,]+))@', $txt, $matches, PREG_SET_ORDER);
+
+  foreach ($matches as $m) {
+      $data[$m[1]] = $m[3] ? $m[3] : $m[4];
+      unset($needed_parts[$m[1]]);
+  }
+
+  return $needed_parts ? false : $data;
+}
+
+return auth();
--- html/index.php
+++ html/index.php
@@ -1,15 +1,64 @@
+<HTML>
 <?php 
 
 if ((include 'common.php') != TRUE ) {
 	echo "\necho 'Error with loading common functions file'";
 	return false;
 }
+$user = auth();
+global $user;
+echoDebug('index::user',$user,9);
 
 ### MAIN ###
-$result = db_query("SELECT * from reports", array());
-echoDebug("index::init", $result, 0);
+
+function get_containers() { 
+  global $user;
+  $result = db_query("SELECT * FROM container WHERE warehouse_id IN (SELECT id FROM warehouse WHERE id=?)", array($user[id]));
+  echoDebug("index::init", $result, 0);
+  $fetchAll=$result[stmt]->fetchAll();
+  array_unshift($fetchAll, $result[fetch]);
+  return $fetchAll;
+}
+function get_warehouse() { 
+  global $user;
+  $result = db_query("SELECT * FROM warehouse WHERE id=?", array($user[id]));
+  echoDebug("index::init", $result, 0);
+  $fetchAll=$result[stmt]->fetchAll();
+  array_unshift($fetchAll, $result[fetch]);
+  return $fetchAll;
+}
+function get_reports() { 
+  global $user;
+  $result = db_query("SELECT * FROM report WHERE container_id IN (SELECT warehouse_id FROM container WHERE id IN (SELECT id FROM warehouse WHERE id=?))", array($user[id]));
+  echoDebug("index::init", $result, 0);
+  $fetchAll=$result[stmt]->fetchAll();
+  array_unshift($fetchAll, $result[fetch]);
+  return $fetchAll;
+}
+
+if (isset($_GET['action'])) { 
+  if ($_GET['action'] == 'get_containers') {
+    echo json_encode(get_containers());
+  }
+
+  if ($_GET['action'] == 'get_warehouse') {
+    echo json_encode(get_warehouse());
+  }
+
+  if ($_GET['action'] == 'get_user') {
+    echo json_encode($user);
+  }
+
+  if ($_GET['action'] == 'get_reports') {
+    echo json_encode(get_reports());
+  }
+}
+  
+  
+
+
 #phpinfo();
 
 
-
 ?>
+<HTML>
