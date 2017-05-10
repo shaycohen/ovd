9e74df07e483554c0c8e1ec6cc2ee697518f1b84
Shay Cohen <shay.cohen@sap.com>
2017-05-10 12:00:02 +0300

    upload image init

---
+++ html/report.php
@@ -0,0 +1,52 @@
+<?php
+
+if ((include 'common.php') != TRUE ) {
+	echo "\necho 'Error with loading common functions file'";
+	return false;
+}
+
+$target_dir = "/var/www/html/uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+
+echo '<pre>';
+if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_file)) {
+    echo "File is valid, and was successfully uploaded.\n";
+} else {
+    echo "Possible file upload attack!\n";
+}
+
+echo 'Here is some more debugging info:';
+echoDebug("upload::post_upload", $_FILES, 5);
+
+print "</pre>";
+
+?>
